7d07763f950263e800595928621dbe43a0c43dbb
천리마민방위 잎사귀 <48374148+cosplex01@users.noreply.github.com>
2020-03-18 01:18:45 +0900

    캠코더 입력 데이터를 영상파일로 저장하기
    1. d드라이브에 opencv를 설치하였음
    2. 동적으로 lib파일을 연결하였기에 몇몇 머신에서는 정상작동이 되지않음을 발견함

---
+++ camSave.cpp
@@ -0,0 +1,52 @@
+﻿#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+//#include <opencv2/imgproc.hpp> // for cvtColor
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	//영상을 담을 메트릭스 생성
+	Mat frame;
+	//웹켐 캡처용 변수 지정
+	VideoCapture cap(0);
+	if (!cap.isOpened())
+	{
+		cerr << "카메라를 열 수 없습니다." << endl;
+		return -1;
+	}
+	//캡쳐한영상을 저장할 세부변수 지정
+	int fps = 30;
+	int width = cap.get(CAP_PROP_FRAME_WIDTH);
+	int height = cap.get(CAP_PROP_FRAME_HEIGHT);
+	int fourcc = VideoWriter::fourcc('M', 'J', 'P', 'G');
+	//캡쳐한영상을 담을 변수지정
+	VideoWriter outputVideo;
+	outputVideo.open("output.avi", fourcc, fps, Size(width, height), true);
+	
+	while (1)
+	{
+		cap.read(frame);
+		if (frame.empty())
+		{
+			cerr << "캡쳐 실패" << endl;
+			break;
+		}
+
+		//cvtColor(frame, frame, COLOR_BGR2GRAY);
+
+		imshow("Live", frame);
+		// 메트릭스에 저장된 켑쳐영상을 저장함
+		outputVideo.write(frame);
+
+		if (waitKey(1) >= 0)
+			break;
+	}
+	// 캡쳐영상 저장작업 헤제
+	outputVideo.release();
+
+	return 0;
+}
