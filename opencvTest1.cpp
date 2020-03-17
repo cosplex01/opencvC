f41689f37167ea97107768698100dbae8a14ed1b
천리마민방위 잎사귀 <48374148+cosplex01@users.noreply.github.com>
2020-03-18 01:24:46 +0900

    영상파일을 읽어들여서 생성한 창에서 재생하기
    1. d드라이브에 opencv를 설치하였습니다
    2. 읽어들일 영상파일명은 output.avi로 지정하였습니다 [추후 소스수정시 변경가능합니다]
    3. 영상파일의 포멧은 avi로 합니다
    4. 일반적인 머신에서 정상작동함을 확인하였습니다

---
+++ opencvTest1.cpp
@@ -0,0 +1,37 @@
+﻿#include <opencv2/core.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main()
+{
+	// 메트릭스에 영상파일을 담을 구조체 생성
+	Mat frame;
+	// 영상파일 이름을 지정한다[읽어들일 파일명]
+	VideoCapture cap("output.avi");
+	if (!cap.isOpened())
+	{
+		cerr << "카메라를 열 수 없습니다." << endl;
+		return -1;
+	}
+	
+	while (1)
+	{
+		// 구조체에 영상파일을 담는다
+		cap.read(frame);
+		if (frame.empty())
+		{
+			cerr << "캡쳐 실패" << endl;
+			break;
+		}
+		// 구조체에 담은내용을 창에 보여준다
+		imshow("Live", frame);
+		// 어떠한 키보드입력을 받으면 루프문 벗어남
+		if (waitKey(30) >= 0)
+			break;
+	}
+	return 0;
+}
